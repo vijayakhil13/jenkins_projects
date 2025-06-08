eeecdc75eb89194b742fa480a05c0b123b043617
venkat vineeth <venkatvineeth51@gmail.com>
2025-06-08 13:05:59 +0530

    added 2tierapp

---
+++ 2tierapp/Backend/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
+
+# SQLite DB for demo; can use PostgreSQL/MySQL if needed
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///items.db'
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = Item.query.all()
+    return jsonify([{'id': item.id, 'name': item.name} for item in items])
+
+@app.route('/items', methods=['POST'])
+def add_item():
+    data = request.get_json()
+    new_item = Item(name=data['name'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify({'id': new_item.id, 'name': new_item.name}), 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
